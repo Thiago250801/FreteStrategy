9a9e4f1190bee15eb2c675df0181b64296d345f1
Thiago Arthur <thlimabsgim@gmail.com>
2024-11-08 19:41:38 -0400

    Renomeando a instancia

--- src/org/padroes/aula06/Cliente.java
+++ src/org/padroes/aula06/Cliente.java
@@ -4,13 +4,13 @@ public class Cliente {
     public static void main(String[] args) {
         FreteContext context = new FreteContext();
         FreteExpresso freteExpresso = new FreteExpresso();
-        FreteNormal freteNomal = new FreteNormal();
+        FreteNormal freteNormal = new FreteNormal();
         FreteSedex freteSedex = new FreteSedex();
 
         context.setFreteStrategy(freteExpresso);
         context.calcular(200, 32);
         System.out.println("------------------------------------");
-        context.setFreteStrategy(freteNomal);
+        context.setFreteStrategy(freteNormal);
         context.calcular(200,32);
         System.out.println("------------------------------------");
         context.setFreteStrategy(freteSedex);
